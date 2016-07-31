bef2dfc41e38e1dc8bc7125eb1099bd67a18e1a8
Erika Cornejo <eriks_dance@hotmail.com>
2016-07-31 19:41:56 +0000

    changes 2

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160729200747) do
+ActiveRecord::Schema.define(version: 20160731183058) do
 
   create_table "leads", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20160729200747) do
     t.datetime "updated_at", null: false
     t.string   "location"
     t.integer  "phone"
+    t.date     "date"
   end
 
 end
